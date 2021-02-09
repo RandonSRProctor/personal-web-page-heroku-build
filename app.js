3e406c697607a59a4f560d2b31535904fed892a0
RandonSRProctor <randyproctor@gmail.com>
2021-02-09 15:46:21 -0500

    First Commit

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const sendGrid = require('@sendGrid/mail');
+const path = require('path');
+
+require('dotenv').config({path: './sendgrid.env'})
+
+const app = express();
+
+//tells app to look for build folder that contains build of react app
+app.use(express.static(path.join(__dirname, 'build')))
+
+
+//points to index.html
+app.get('/', function(req,res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+
+app.get('/api', (req, res, next) => {
+    res.send('API Status: Running')
+});
+
+app.post('/api/email', (req, res, next) => {
+debugger
+    sendGrid.setApiKey(process.env.SENDGRID_API_KEY);
+    
+    const msg = {
+        to:'randyproctor@gmail.com',
+        from: 'contact@randyproctor.net', 
+        subject: `Website Contact from ${req.body.email}`,
+        text: req.body.message
+    }
+
+    sendGrid.send(msg)
+        .then(result => {
+            res.status(200).json({
+                success: true
+            });
+        })
+        .catch(err => {
+
+            console.log('error: ', err);
+            res.status(401).json({
+                success: false
+            });
+
+        });
+
+});
+
+
+app.listen(3030, '0.0.0.0');
---
+++ build/precache-manifest.18985f9c9577961523b4ed1a978bb0b9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69d8d54d4cef8fd058cd4dd872400dd8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b323b8496c054de1a63d",
+    "url": "./static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "e7307fc8c5eeb7560460",
+    "url": "./static/css/main.3a05ba31.chunk.css"
+  },
+  {
+    "revision": "b323b8496c054de1a63d",
+    "url": "./static/js/2.76db1734.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.76db1734.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7307fc8c5eeb7560460",
+    "url": "./static/js/main.7d3429fd.chunk.js"
+  },
+  {
+    "revision": "815c93d92e076566805a",
+    "url": "./static/js/runtime-main.b787ce41.js"
+  },
+  {
+    "revision": "f6075f0ff073e2af64556d215e0ebde0",
+    "url": "./static/media/MathPractice.f6075f0f.png"
+  },
+  {
+    "revision": "c440fb602e44d369997417be0c4a1c26",
+    "url": "./static/media/TetrisColor.c440fb60.png"
+  },
+  {
+    "revision": "d7a10745d5aaafb1ee507b488b6853ad",
+    "url": "./static/media/WeatherApp.d7a10745.png"
+  }
+]);
